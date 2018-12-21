ba05080cbbcd241e0cfb965e839af1358d40ae54
Victor Dumas <victordumas@gmail.com>
2018-12-21 16:20:31 -0300

    init project

---
+++ src/main/java/com/sagant/springpc/SpringpcApplication.java
@@ -0,0 +1,14 @@
+package com.sagant.springpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringpcApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringpcApplication.class, args);
+  }
+
+}
+
